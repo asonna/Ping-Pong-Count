df136dc0a9a6c83b8a9781e6a4a67f049faae26b
Annie Sonna <nsab42@yahoo.com>
2016-10-26 21:20:59 -0700

    result now changes 3, 5 & 15 multiples into ping, pong, pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 // business logic
 // var number = [1,2,3,4,5,6]
 var userInputs = 0;
-var numbers = 0;
+var textResult = "";
 var numberArray = [];
 // replace.length = parseInt(userInputs);
 function replace(userInputs) {
   for(var i=1; i<=userInputs; i++) {
-    numberArray.push(numbers = i);
+    if(i%15 === 0) {
+      textResult = "PingPong";
+    } else if (i%3 === 0) {
+      textResult = "Ping";
+    } else if (i%5 === 0) {
+      textResult = "Pong";
+    } else {
+      textResult = i;
+    }
+    numberArray.push(textResult);
 //     // if(text%15) {
 //     //   text = PingPong;
 //     // } else if (text%3) {
@@ -29,7 +38,6 @@ $(document).ready(function() {
   var numberSelection = $(".numberEntry").val();
   var result = replace(numberSelection);
 // consoleLog(result);
-//   $(result).show();
 
   $(".output").show();
   $(".resultHere").text(result);
